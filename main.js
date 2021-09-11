95c43cf9b7332efb3a16ca2d324c264d1d3d90eb
VinicioRuaro <50388936+VinicioRuaro@users.noreply.github.com>
2021-09-11 18:10:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,105 @@
+var myRectangle = {
+  x: 630,
+  y: 70,
+  width: 50,
+  height: 50,
+  color: "green"
+};
+
+
+
+var lastTime = 0;
+
+function calculateFps() {
+  var now = new Date,
+  fps = 1000 / (now - lastTime);
+  lastTime = now;
+  return fps;
+}
+
+function drawFPS(ms){
+  var now = new Date;
+  var fps = calculateFps();
+  if (now - fpsUpdate > ms) {
+    fpsUpdate = now;
+    fpsValue = fps;
+  }
+  context.fillStyle = 'pink';
+  context.font = '20px monospace';
+  context.fillText(fpsValue.toFixed() + ' fps', 10, 20);
+}
+
+function drawBackground(){
+  context.drawImage(sky,0,0, canvas.width, canvas.height);
+}
+
+
+function update(){
+  var newX = 0;
+    if (reverse){
+      newX = myRectangle.x-10;
+      if(newX > 0+120) {
+        myRectangle.x = newX;
+        
+      } else {
+         reverse = false;
+        myRectangle.x= 630;
+        myRectangle.y= 70;
+      }
+    } 
+    else {
+      newX = myRectangle.x+10;
+      if(newX < canvas.width - myRectangle.width-120) {
+        myRectangle.x = newX;
+      } else {
+        reverse = true;
+        myRectangle.x= 630;
+        myRectangle.y= 420;
+      }
+      }
+}
+
+
+
+function draw(){
+  context.beginPath();
+  context.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
+  if(reverse){
+  context.fillStyle = 'purple';
+  }else{
+    context.fillStyle = 'blue';
+  }
+  context.fill();
+  context.lineWidth = 1;
+  context.strokeStyle = 'black';
+  context.stroke();
+}
+
+function animate(time) {
+  context.clearRect(0,0,canvas.width,canvas.height);
+  drawBackground();
+  update();
+  draw();
+  drawFPS(1000);
+  window.requestNextAnimationFrame(animate);
+}
+
+var canvas = document.getElementById("animation-screen");
+canvas.width = window.innerWidth-25;
+canvas.height = window.innerHeight-25;
+var context = canvas.getContext("2d");
+var reverse = false;
+var inverter = true;
+var fpsUpdate = 0;
+var fpsValue = 0;
+var sky = new Image();
+    sky.src = "fundo 1.jpg";
+
+
+window.requestNextAnimationFrame =  window.requestAnimationFrame
+  || window.webkitRequestAnimationFrame
+  || window.mozRequestAnimationFrame
+  || window.oRequestAnimationFrame
+  || window.msRequestAnimationFrame;
+
+animate(new Date());
